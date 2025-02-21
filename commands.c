57712a23aa07a12e6b01254de673c49b6f4eeaa6
Lumi Kalt <lumi.mia.kalt@gmail.com>
2025-02-21 00:47:04 +0000

    improve the commands and start implementing them

--- commands.c
+++ commands.c
@@ -1,16 +1,108 @@
 #include "./commands.h"
+#include "./types.h"
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void ndn_help() {
   printf("Commands:\n"
-         "  (h)  help - show this message\n"
-         "  (j)  join <net> - join the network (000-999)\n"
-         "  (st) show_topology - show the network topology\n"
-         "  (x)  exit - close the program\n");
+         "\t(h)  help - show this message\n"
+         "\t(j)  join <net> - join the network (000-999)\n"
+         "\t(dj) direct join <net> <IP> <TCP> - directly join a network\n"
+         "\t(c)  create <name> - create an object\n"
+         "\t(dl) delete <name> - delete an object\n"
+         "\t(r)  retrieve <name> - retrieve an object\n"
+         "\t(st) show_topology - show the neighbourhood's topology\n"
+         "\t(sn) show_names - show the object names in this node\n"
+         "\t(si) show_interest_table - show the interest table\n"
+         "\t(l)  leave - leave the network\n"
+         "\t(x)  exit - close the program\n");
 }
 
-void ndn_join(u16 net) { printf("Joining network %d\n", net); }
+void ndn_join(Server *s, u16 net) {}
+
+void ndn_direct_join(u16 net, char *connectIP, char *connectTCP) {
+  printf("Directly joining network %d, linking to %s:%s\n", net, connectIP,
+         connectTCP);
+}
+
+void ndn_create(const char *name) { printf("Creating object %s\n", name); }
+
+void ndn_delete(const char *name) { printf("Deleting object %s\n", name); }
+
+void ndn_retrieve(const char *name) { printf("Retrieving object %s\n", name); }
 
 void ndn_show_topology() { printf("Showing network topology\n"); }
 
+void ndn_show_names() { printf("Showing object names\n"); }
+
+void ndn_show_interest_table() { printf("Showing interest table\n"); }
+
+void ndn_leave() { printf("Leaving network\n"); }
+
 void ndn_exit() { printf("Exiting program\n"); }
+
+/* Server Comms internals */
+
+
+
+/// Register the node in the network, and check if the server accepted the node
+/// entry.
+void ndn_register(Server *s, u16 net) {
+  ssize_t n;
+  char buffer[256];
+
+  sprintf(buffer, "REG %03d %s %s", net, s->IP, s->TCP);
+
+  if (n = sendto(s->serverfd, buffer, sizeof(buffer), 0, s->udp->ai_addr,
+                 s->udp->ai_addrlen),
+      n <= 0)
+    perror("ERR: Failed to send the join request"); // TODO: clean up
+  else
+    printf("OK: Requested join to net %03d\n", net);
+
+  /* Wait for the OK */
+
+  const char *ok = "OKREG";
+  char response[6];
+
+  if (n = recvfrom(s->serverfd, response, sizeof(response), 0, s->udp->ai_addr,
+                   &s->udp->ai_addrlen),
+      n <= 0)
+    perror("ERR: No response from the server"); // TODO: clean up
+  else if (strncmp(response, ok, 6) != 0)
+    printf("ERR: Server refused the connection to net %03d\n", net);
+  else
+    printf("OK: Successfully joined network %d\n", net);
+}
+
+/// Unregister the node from the network, and check if the server accepted the
+/// node exit.
+void ndn_unregister(Server *s, Adjacencies *adj, u16 net) {
+  ssize_t n;
+  char buffer[256];
+
+  sprintf(buffer, "UNREG %03d %s %s", net, s->IP, s->TCP);
+
+  if (n = sendto(s->serverfd, buffer, sizeof(buffer), 0, s->udp->ai_addr,
+                 s->udp->ai_addrlen),
+      n <= 0)
+    perror("ERR: Failed to send the leave request"); // TODO: clean up
+  else
+    printf("OK: Requested leave from net %03d\n", net);
+
+  /* Wait for the OK */
+
+  const char *ok = "OKUNREG";
+  char response[8];
+
+  if (n = recvfrom(s->serverfd, response, sizeof(response), 0, s->udp->ai_addr,
+                   &s->udp->ai_addrlen),
+      n <= 0)
+    perror("ERR: No response from the server"); // TODO: clean up
+  else if (strncmp(response, ok, 8) != 0)
+    printf("ERR: Server refused the connection to net %03d\n", net);
+  else
+    printf("OK: Successfully left network %d\n", net);
+}
--- commands.h
+++ commands.h
@@ -2,10 +2,26 @@
 
 #include "./types.h"
 
+#include <netdb.h>
+
 void ndn_help();
 
-void ndn_join(u16 net);
+void ndn_join(Server *s, u16 net);
+
+void ndn_direct_join(u16 net, char *connectIP, char *connectTCP);
+
+void ndn_create(const char *name);
+
+void ndn_delete(const char *name);
+
+void ndn_retrieve(const char *name);
 
 void ndn_show_topology();
+
+void ndn_show_names();
+
+void ndn_show_interest_table();
+
+void ndn_leave();
 
 void ndn_exit();
