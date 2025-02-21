117b8895470c3a3b4675100d4f2d6fcce118894b
Lumi Kalt <lumi.mia.kalt@gmail.com>
2025-02-21 00:48:49 +0000

    start implementing the server nodes request

--- commands.c
+++ commands.c
@@ -45,7 +45,83 @@ void ndn_exit() { printf("Exiting program\n"); }
 
 /* Server Comms internals */
 
+typedef struct {
+  char *IP;
+  char *TCP;
 
+  // Sadly this needs to be dynamic, the response doesn't include the number of
+  // nodes
+  usize size;
+  usize capacity;
+} NetNode;
+
+/// Request the list of nodes in the network
+NetNode *ndn_nodes(Server *s, u16 net) {
+  NetNode *nodes = malloc(10 * sizeof(NetNode));
+  nodes->size = 0;
+  nodes->capacity = 10;
+
+  ssize_t n;
+  char buffer[256];
+
+  sprintf(buffer, "NODES %03d", net);
+
+  if (n = sendto(s->serverfd, buffer, sizeof(buffer), 0, s->udp->ai_addr,
+                 s->udp->ai_addrlen),
+      n <= 0)
+    perror("ERR: Failed to send the NODES request"); // TODO: clean up
+  else
+    printf("OK: Requested nodes for net %03d\n", net);
+
+  /* Wait for the OK */
+
+  char ok[15];
+  sprintf(ok, "NODESLIST %03d\n", net);
+
+  char response[15];
+  if (n = recvfrom(s->serverfd, response, sizeof(response), 0, s->udp->ai_addr,
+                   &s->udp->ai_addrlen),
+      n <= 0)
+    perror("ERR: No response from the server"); // TODO: clean up
+  else if (strncmp(response, ok, 15) != 0)
+    printf("ERR: Server response did not match the spec");
+  else
+    printf("OK: Receiving nodes in net %d\n", net);
+
+  /* Get the nodes */
+
+  while (1) {
+    char response[256];
+    // Check if there are no more bytes to read
+    n = recvfrom(s->serverfd, response, sizeof(response), 0, s->udp->ai_addr,
+                 &s->udp->ai_addrlen);
+    if (n <= 0) {
+      if (n == 0) {
+        printf("OK: Finished reading nodes\n");
+      } else {
+        perror("ERR: No response from the server"); // TODO: clean up
+      }
+      break;
+    }
+
+    // Parse the node
+    char *IP = strtok(response, " ");
+    char *TCP = strtok(NULL, " ");
+
+    // Check if we need to resize the array
+    if (nodes->size == nodes->capacity) {
+      nodes->capacity *= 2;
+      nodes = realloc(nodes, nodes->capacity * sizeof(NetNode));
+    }
+
+    // Add the node to the array
+    strcpy(nodes[nodes->size].IP, IP);
+    strcpy(nodes[nodes->size].TCP, TCP);
+    nodes->size++;
+  }
+
+  return nodes;
+}
 
 /// Register the node in the network, and check if the server accepted the node
 /// entry.
