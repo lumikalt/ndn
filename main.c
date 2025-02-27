28e3eed1a372818fc6827cc80b88fb935d344e58
your_username <your_email@example.com>
2025-02-27 13:47:17 +0000

    working on goated select logic

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 // #include "./input.h"
 #include "./types.h"
-#include "util.h"
 
 #include <arpa/inet.h>
 #include <netdb.h> // for AI_PASSIVE
@@ -30,7 +29,7 @@ int main(int argc, char *argv[]) {
   usize cache = atoi(argv[1]);
   char *IP = argv[2];
   char *TCP = argv[3];
-  char *regIP = argc > 4 ? argv[4] : "193.136.138.142";
+  char *regIP = argc > 4 ? argv[4] : INADDR_ANY; //"193.136.138.142"; //NOTE: foi so para debug, depois tiro
   char *regUDP = argc > 5 ? argv[5] : "59000";
 
   /* Connect via UDP to the server */
@@ -43,8 +42,12 @@ int main(int argc, char *argv[]) {
   udp_hints.ai_family = AF_INET;
   udp_hints.ai_socktype = SOCK_DGRAM;
 
-  if (serverfd = socket(AF_INET, SOCK_DGRAM, 0), serverfd != 0)
-    return perror("FATAL: Failed to create UDP socket"), 1;
+  serverfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (serverfd == -1) {
+    perror("FATAL: Failed to create UDP socket");
+    return 1;
+  }
+
 
   if (err = getaddrinfo(regIP, regUDP, &udp_hints, &udp), err != 0)
     return perror("FATAL: Failed to get address info"), 1;
@@ -78,8 +81,10 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
+/*
   // No longer needed, so we free the structure
-  freeaddrinfo(udp);
+  if (udp != NULL)
+    freeaddrinfo(udp);
 
   // Start listening for incoming connections
   if (listen(listener_fd, 5) == -1) {
@@ -88,29 +93,83 @@ int main(int argc, char *argv[]) {
   }
 
   printf("Server is listening on port %s...\n", regUDP);
-
+*/
   // fd_set master_fds, read_fds; // File descriptor sets
   // int max_fd,counter;
   // FD_ZERO(&master_fds);
   // FD_SET(listener_fd, &master_fds);
   // max_fd = listener_fd;
 
-  fd_set read_fds;
-  int bytes_read, max_fd = listener_fd;
+  fd_set read_fds, testfds;
+  int bytes_read, max_fd, result, errorcode;
+  char prt_str[90];
   char buffer[128];
+  struct timeval timeout;
+  struct sockaddr_in udp_useraddr;
+  socklen_t addrlen;
 
+
+  FD_ZERO(&read_fds); //to clear the set of discriptiors
+  FD_SET(STDIN_FILENO, &read_fds); //add the keyboard to the set
+  FD_SET(listener_fd, &read_fds); //add a fd to the set
 
   //loop waiting in case of an input
   while (1) {
-    FD_ZERO(&read_fds);
-    FD_SET(listener_fd, &read_fds);
-    FD_SET(STDIN_FILENO, &read_fds);
 
+    max_fd = listener_fd; //reload mask
+    memset((void *)&timeout, 0, sizeof(timeout));
+    timeout.tv_sec = 10;
+
+
+    result = select(max_fd + 1, &testfds, (fd_set *)NULL, (fd_set *)NULL, (struct timeval *)&timeout);
+    printf("byte: %d\n", ((char *)&testfds)[0]);
+
+    switch (result) {
+      case 0:
+        printf("\n----------------Timeout event----------------\n");
+        break;
+      case -1:
+        perror("select");
+        exit(1);
+      default:
+        if (FD_ISSET(0, &testfds)) {
+          fgets(buffer, 50, stdin);
+          printf("\nInput at keyboard: %s\n", buffer);
+          if (!memcmp(buffer, "_STOP_", 5)) {
+            write(1, "Terminating\n", 12);
+            exit(0);
+          }
+        }
+
+        if (FD_ISSET(listener_fd, &testfds)) {
+          addrlen = sizeof(udp_useraddr);
+        int ret = recvfrom(listener_fd, prt_str, 80, 0, (struct sockaddr *)&udp_useraddr, &addrlen);
+          if (ret > 0) {
+            if (strlen(prt_str) > 0)
+              prt_str[ret - 1] = '\0';
+
+            printf("----UDP socket: %s\n", prt_str);
+
+            if (!memcmp(prt_str, "_STOP_", 5)) {
+              write(1, "Terminating\n", 12);
+              exit(0);
+            }
+          }
+        }
+    }
+
+
+
+  }
+
+
+/*
     if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) == -1) {
       perror("select");
       exit(1);
     }
 
+    //to check the readiness of a discriptior
     if (FD_ISSET(STDIN_FILENO, &read_fds)) {
       if ((bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer)-1)) <= 0) {
         perror("read");
@@ -123,7 +182,7 @@ int main(int argc, char *argv[]) {
       process_input_commands(buffer);
     }
   }
-
+*/
   // if (listenerfd = socket(AF_INET, SOCK_STREAM, 0), listenerfd != 0)
   //   return perror("FATAL: Failed to create TCP socket"), 1;
 
