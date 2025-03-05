f86298e024bcae8c053b3e44daaeb17cad75e001
Lumi Kalt <lumi.mia.kalt@gmail.com>
2025-03-05 13:10:36 +0000

    test for NODELIST

--- test_server.c
+++ test_server.c
@@ -1,18 +1,67 @@
 // Test server for the UDP communication
 
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 int main() {
-    // Create a UDP server
-    int server_fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (server_fd == -1) {
-        perror("socket");
-        exit(1);
-    }
+  // Create a UDP server
+  int server_fd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (server_fd == -1) {
+    perror("socket");
+    exit(1);
+  }
 
-    struct sockaddr_in server_addr;
+  struct sockaddr_in server_addr;
 
+  // use getaddrinfo to get the address info
 
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(59000);
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+
+  // Bind the socket to the address
+
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) ==
+      -1) {
+    perror("bind");
+    exit(1);
+  }
+
+  // Receive data from the client
+  char buffer[128];
+  struct sockaddr_in client_addr;
+  socklen_t client_addr_len = sizeof(client_addr);
+
+  int bytes_received =
+      recvfrom(server_fd, buffer, sizeof(buffer), 0,
+               (struct sockaddr *)&client_addr, &client_addr_len);
+  if (bytes_received == -1) {
+    perror("recvfrom");
+    exit(1);
+  }
+
+  printf("Received %d bytes from %s:%d\n", bytes_received,
+         inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+  // Send response to the client
+  char response[] = "NOTELIST 123"
+                    "123.456.789 321431"
+                    "879.423.789 214354"
+                    "432.456.890 408321";
+
+  int bytes_sent = sendto(server_fd, response, sizeof(response), 0,
+                          (struct sockaddr *)&client_addr, client_addr_len);
+  if (bytes_sent == -1) {
+    perror("sendto");
+    exit(1);
+  }
+
+  printf("Sent %d bytes to %s:%d\n", bytes_sent,
+         inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+  close(server_fd);
+  return 0;
 }
